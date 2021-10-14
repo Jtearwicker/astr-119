b0b24ca2a7033c20f88288d1259736f6c99a62ed
JT Earwicker <85366224+Jtearwicker@users.noreply.github.com>
2021-10-14 09:41:29 -0700

    Add files via upload

---
+++ astr-119-hw-2/if_else_control_corrected.py
@@ -0,0 +1,20 @@
+def flow_control(k):
+	if(k==0): 
+		s="Variable k = %d equals 0." %k
+	elif(k==1):
+		s="Variable k = %d equals 1." %k
+	else: 
+		s="Variable k= %d does not equal 0 or 1." %k
+
+	print(s)		#function takes in variable k and checks if it is 0, 1, or neither, then prints the result
+
+def main(): #test each case (0,1,other)
+	i=0 
+	flow_control(i)
+	i=1
+	flow_control(i)
+	i=5
+	flow_control(i)
+
+if __name__=="__main__":
+	main()
